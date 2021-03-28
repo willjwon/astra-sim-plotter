855d0bc17e17380ec0005e4f3c8b6db2b6d9472c
Will Won <william.won@gatech.edu>
2021-03-28 02:51:14 -0400

    Add license

--- layer_wise_plotter.py
+++ layer_wise_plotter.py
@@ -1,3 +1,8 @@
+"""
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+"""
+
 from csv_loader import CSVLoader
 from breakdown_plot_drawer import *
 from grid_plot_drawer import *
